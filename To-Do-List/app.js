5f2bc8bca919e903de2f26528dacfde9d0b6810b
Yerin Kim <87409442+Yerineee@users.noreply.github.com>
2023-02-07 22:44:58 +0900

    Feat: "Add items to the local storage"
    추가 버튼 클릭 시 로컬 스토리지에 항목 추가

--- To-Do-List/app.js
+++ To-Do-List/app.js
@@ -2,6 +2,12 @@ const addBtn = document.querySelector(".add-wrap");
 const toDoList = document.querySelector(".todo-list");
 const toDoInput = document.querySelector(".todo-input");
 
+const toDos = [];
+
+function getToDos() {
+  return JSON.parse(JSON.stringify(localStorage.getItem("toDo")));
+}
+
 // 로컬 스토리지에 항목 저장
 function saveToDo(newToDo) {
   localStorage.setItem("toDos", newToDo);
@@ -16,7 +22,7 @@ function paintToDo(newToDo) {
   checkbox.classList.add("checkbox");
 
   const checkboxImg = document.createElement("img");
-  checkboxImg.src = "./images/checkbox-btn-unchecked.png";
+  checkboxImg.src = "./images/checkbox-btn.png";
   checkbox.addEventListener("click", handleCheckbox);
 
   const checkboxText = document.createElement("div");
@@ -42,30 +48,26 @@ function paintToDo(newToDo) {
   toDoList.appendChild(toDoItem);
 }
 
-// 체크박스 클릭했을 때 스타일 적용
+// 체크박스 클릭 시 스타일 적용
 function handleCheckbox(event) {
   const checkboxImg = event.target;
   const checkbox = checkboxImg.parentElement;
   const toDoItem = checkbox.parentElement;
 
   if (toDoItem.classList.contains("checked")) {
-    checkboxImg.src = "./images/checkbox-btn-unchecked.png";
-
     toDoItem.classList.remove("checked");
   } else {
-    checkboxImg.src = "./images/checkbox-btn-checked.png";
-
     toDoItem.classList.add("checked");
   }
 }
 
-// 항목 옆의 ✖ 버튼 누르면 목록에서 해당 항목 삭제
+// 항목 옆의 ✖ 버튼 클릭 시 목록에서 해당 항목 삭제
 function removeToDo(event) {
   const toDoItem = event.target.parentElement;
   toDoList.removeChild(toDoItem);
 }
 
-// 추가 버튼 눌렀을 때
+// 추가 버튼 클릭 시
 function handleAddToDo(event) {
   event.preventDefault();
 
@@ -73,6 +75,9 @@ function handleAddToDo(event) {
   toDoInput.value = "";
 
   paintToDo(newToDo);
+
+  toDos.push(newToDo);
+  localStorage.setItem("toDo", toDos);
 }
 
 addBtn.addEventListener("click", handleAddToDo);
